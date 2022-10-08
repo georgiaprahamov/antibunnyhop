927a16665cf71577cf255c7f01188ae82aaac9d9
Geshata <64840882+g3shaa@users.noreply.github.com>
2022-10-08 22:45:42 +0300

    Update client.lua
    Rewriting the script to work in a better way

--- client.lua
+++ client.lua
@@ -1,29 +1,18 @@
-
-
-Citizen.CreateThread(function()
-  local Jump = 0
-  while true do
-
-      Citizen.Wait(1)
-
-      local ped = PlayerPedId()
-
-      if IsPedOnFoot(ped) and not IsPedSwimming(ped) and (IsPedRunning(ped) or IsPedSprinting(ped)) and not IsPedClimbing(ped) and IsPedJumping(ped) and not IsPedRagdoll(ped) then
-
-        Jump = Jump + 1
-
-          if Jump == Config.NumberJump then
-
-              SetPedToRagdoll(ped, 5000, 1400, 2)
-              exports['mythic_notify']:DoHudText('error', 'ТВЪРДЕ ИЗМОРЕН СИ!') 
-              Jump = 0
-
-          end
-
-      else 
-
-          Citizen.Wait(500)
-          
-      end
-  end
+local QBCore = exports['qb-core']:GetCoreObject()
+CreateThread(function()
+    while true do
+        Wait(100)
+        local ped = PlayerPedId()
+        if IsPedOnFoot(ped) and not IsPedSwimming(ped) and (IsPedRunning(ped) or IsPedSprinting(ped)) and not IsPedClimbing(ped) and IsPedJumping(ped) and not IsPedRagdoll(ped) then
+            local chance_result = math.random()
+            if chance_result < Config.RagdollChance then 
+                Wait(600)
+                ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.08)
+                QBCore.Functions.Notify('ТВЪРДЕ ИЗМОРЕН СИ!', 'error', 2500)
+                SetPedToRagdoll(ped, 5000, 1, 2)
+            else
+                Wait(2000)
+            end
+        end
+    end
 end)
